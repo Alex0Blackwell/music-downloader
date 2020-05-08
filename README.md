621fd05413391396d2fdb42b715be8a062f4a309
alex blackwell <alex.blackwell314@gmail.com>
2020-05-08 16:11:07 -0700

    Added Spotify playlist scraping and a folder management update

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # music-downloader README
-By using YouTube-dl and python, the names of songs can be entered in a .txt file and the program will search YouTube for the top lyrical video and download the audio to the user's music folder.
+By using YouTube-dl, Selenium, Beautifulsoup, and python, the names of songs can be entered in a .txt file, or scraped from a Spotify playlist, and the program will search YouTube for the top lyrical video and download the audio to the user's Music folder in a folder named after the playlist.
 
 ### Installation
-This program uses the youtube-dl API to fetch audio so it must be installed.
+This program uses the youtube-dl, Selenium, and the beatifulsoup4 API to fetch and download YouTube audio so it must be installed.
 
-For UNIX users (Linux / OS X ...):
+For UNIX users (Linux / OS X ...), youtube-dl can be installed with:
 
     sudo curl -L https://yt-dl.org/downloads/latest/youtube-d -o /usr/local/bin/youtube-dl
     sudo chmod a+rx /usr/local/bin/youtube-dl
 
-Alternatively, with pip:
+Alternatively, with pip, youtube-dl, Selenium, and beautifulsoup4 can be installed by:
 
     sudo pip install youtube_dl
+    sudo pip install selenium
+    sudo pip install beautifulsoup4
 
+Note that Selenium requires a driver associated with your browser of choice, which can be downloaded off the Selenium website.
+The path to the driver must be specified, in my code this is done on line 8 and the syntax is slightly different for different browsers. In my case I'm using FireFox, which uses Geckodriver to run Selenium.
+
+Note that a few lines will have to be changed as they reference my file system.
 ### Usage
-To use the program, type in the names of songs you want downloaded into the *songList.txt* file, then run *musicDownloader.py* and the songs will be downloaded to the *~/Music* directory.
+To use the program, type in the names of songs you want downloaded into the *songList.txt* file. Alternatively, paste a link of a Spotify **online** playlist and the program will scrape all the songs. You will have a choice of what method you wish to use when the script is run. To run the program, type *musicDownloader.py* and the songs will be downloaded to the *~/Music/"Playlist name"* directory.
--- musicDownloader.py
+++ musicDownloader.py
@@ -1,5 +1,11 @@
 import youtube_dl
+import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time as t
 
+# note there are a few file paths that you will have to change such as the following
+browser = webdriver.Firefox(executable_path='/home/alex/Downloads/geckodriver')
 
 class MyLogger(object):
     # For debugging and error throwing
@@ -13,9 +19,11 @@ class MyLogger(object):
         print(msg)
 
 
-def download(title):
-    # Downloads a m4a audio file with the provided title
-    outtmpl = '~/Music/' + title + '.%(ext)s'
+def download(title, folder):
+    # Downloads a m4a audio file with the provided title to the provided folder
+    # in the Music directory
+    outtmpl = '~/Music/' + folder + '/' + title + '.%(ext)s'
+
     ydl_opts = {
         'format': 'm4a',
         'outtmpl': outtmpl,
@@ -23,8 +31,12 @@ def download(title):
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        # Get the top result off youtube
-        ydl.extract_info("ytsearch:"+title+"lyrics", download=True)
+        # Get the top result off youtube, let's use Selenium to find the link
+        # and let's use ytdl to download the YouTube video
+        titleSearch = title.replace(" ", "+")
+        browser.get(f"https://www.youtube.com/results?search_query={titleSearch}+lyrics")
+        link = browser.find_element_by_id("thumbnail").get_attribute("href")
+        ydl.download([link])
 
 
 def initialize():
@@ -40,29 +52,73 @@ def initialize():
 
 
 def main():
-    # File minipulation
-    file = open('songList.txt', 'r')
-    numLines = len(file.readlines())
-    file.close()
+    usrChoice = input("Type (1) to download from a Spotify playlist link.\nType (2) to download from the \"songList.txt\" file.\n")
+    if(usrChoice == '1'):
+        # The user wishes to download from the songList.txt file
+        print("Launching Selenium bot-driven driver.")
 
-    if(numLines <= 1):
-        print("You didn't add any songs to the text file.\n"
-              "Open the \"songList.txt\" file and add the names of songs you "
-              "wish to download.")
+        playlistLink = input("Enter the link of the Spotify Playlist you wish to download:\n")
+        print("Requesting webpage.")
+        browser.get(playlistLink)
 
-    elif(numLines > 1):
-        for i in range(1, numLines):
-            file = open('songList.txt', 'r')
-            line = str(file.readlines()[i]).rstrip()
-            file.close()
+        print("Waiting for the page to load js content.")
+        t.sleep(2)
+        content = browser.page_source.encode('utf-8').strip()
+        soup = BeautifulSoup(content, "html.parser")
 
-            print(f"Downloading: {line}...")
-            download(line)
+
+        allSongs = soup.find_all("div", {"class": "tracklist-name"})
+        allArtistsRAW = soup.find_all("a", {"class": "tracklist-row__artist-name-link"})
+
+        allArtists = []
+
+        # for handling features
+        # always get the first artist
+        i = step = 0;
+        while(i < len(allArtistsRAW)):
+            thisArtist = allArtistsRAW[i].parent.parent.parent.text.split(',')
+            if(len(thisArtist) > 1):
+                # then there is a feature
+                i += len(thisArtist) - 1
+            allArtists.append(thisArtist[0])
+            i += 1
+
+        playlistName = soup.find("h1").text
+        numSongs = len(allSongs)
+        # note that if the song is already downloaded ytdl does not download it twice
+        # note that if the file does not exist, ytdl automatically makes it
+        for i in range(numSongs):
+            searchFor = allSongs[i].text + ", " + allArtists[i]
+            print(f"({i+1}/{numSongs}) Downloading {allSongs[i].text} by {allArtists[i]} to {playlistName}...")
+            download(searchFor, playlistName)
             print("Download complete.")
-        print(f"Finished all downloads ({i})")
+        print(f"Completed all downloads ({numSongs})")
 
-    # Erases contents of file so no need to re-download songs
-    initialize()
+    elif(usrChoice == '2'):
+        # The user wishes to download from the songList.txt file
+        # File minipulation
+        file = open('songList.txt', 'r')
+        numLines = len(file.readlines())
+        file.close()
 
+        if(numLines <= 1):
+            print("You didn't add any songs to the text file.\n"
+                  "Open the \"songList.txt\" file and add the names of songs you "
+                  "wish to download.")
+
+        elif(numLines > 1):
+            for i in range(1, numLines):
+                file = open('songList.txt', 'r')
+                line = str(file.readlines()[i]).rstrip()
+                file.close()
+
+                print(f"Downloading: {line}...")
+                download(line, "File-Playlist")
+                print("Download complete.")
+            print(f"Finished all downloads ({i})")
+
+        # Erases contents of file so no need to re-download songs
+        initialize()
 
 main()
+browser.close()  # close the Selenium browser
